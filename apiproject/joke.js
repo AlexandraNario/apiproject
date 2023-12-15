9b2029f8e052e3c25d1ce111588ff9cacb65a9a5
AlexandraNario <142264182+AlexandraNario@users.noreply.github.com>
2023-12-15 00:00:22 -0800

    testing API that doesn't require key

--- apiproject/joke.js
+++ apiproject/joke.js
@@ -2,11 +2,21 @@ import axios from 'axios';
 
 const apiUrl = 'https://official-joke-api.appspot.com/random_joke';
 
-export async function fetchJoke() {
+async function fetchJoke() {
   try {
     const response = await axios.get(apiUrl);
-    console.log(response.data.setup);
-    console.log(response.data.punchline);
+    const jokeSetup = response.data.setup;
+    const jokePunchline = response.data.punchline;
+
+    const jokeSetupElement = document.createElement('p');
+    jokeSetupElement.innerText = jokeSetup;
+
+    const jokePunchlineElement = document.createElement('p');
+    jokePunchlineElement.innerText = jokePunchline;
+
+    const jokeContainer = document.getElementById('joke-container');
+    jokeContainer.appendChild(jokeSetupElement);
+    jokeContainer.appendChild(jokePunchlineElement);
   } catch (error) {
     console.error(error);
   }
